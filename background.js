25d0798acef246d141974d75d61f9f0b967b7cff
Will Wang <001wwang@gmail.com>
2026-01-12 17:04:03 -0500

    Set up window

--- background.js
+++ background.js
@@ -2,3 +2,11 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("Extension installed");
 });
+chrome.commands.onCommand.addListener(async (command) => {
+  if (command === "toggle-cmdk") {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    if (tab?.id) {
+      chrome.tabs.sendMessage(tab.id, { action: "toggle-cmdk" });
+    }
+  }
+});
--- src/background.ts
+++ src/background.ts
@@ -2,3 +2,15 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("Extension installed");
 });
+
+// Listen for keyboard command
+chrome.commands.onCommand.addListener(async (command) => {
+  if (command === "toggle-cmdk") {
+    // Get the active tab
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    if (tab?.id) {
+      // Send message to content script
+      chrome.tabs.sendMessage(tab.id, { action: "toggle-cmdk" });
+    }
+  }
+});
